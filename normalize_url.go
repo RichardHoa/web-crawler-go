3f95faf0663c7f0b748a9a262fee0ea3775524a7
RichardHoa <richardhoa.work@gmail.com>
2024-08-25 20:53:59 +0700

    complete first function

--- normalize_url.go
+++ normalize_url.go
@@ -1,5 +1,17 @@
 package main
 
-func NormalizeURL(url string) string {
-	return url
+import (
+	"net/url"
+	"strings"
+)
+
+func NormalizeURL(inputURL string) string {
+	urlStruct, err := url.Parse(inputURL)
+	if err != nil {
+		// Handle the error
+		return ""
+	}
+	normalizedPath := strings.TrimSuffix(urlStruct.Path, "/")
+	
+	return urlStruct.Host + normalizedPath
 }
--- normalize_url_test.go
+++ normalize_url_test.go
@@ -10,25 +10,36 @@ func TestNormalizeURL(t *testing.T) {
 	tests := []struct {
 		name          string
 		inputURL      string
-		expected      string
+		expectedURL      string
 	}{
 		{
 			name:     "remove scheme",
 			inputURL: "https://blog.boot.dev/path",
-			expected: "blog.boot.dev/path",
+			expectedURL: "blog.boot.dev/path",
 		},
-        // add more test cases here
+		{
+			name:     "URL with slash",
+			inputURL: "https://blog.boot.dev/path/",
+			expectedURL: "blog.boot.dev/path",
+		},
+		{
+			name:     "HTTP url",
+			inputURL: "http://blog.boot.dev/path",
+			expectedURL: "blog.boot.dev/path",
+		},
+		{
+			name:     "HTTP url with slash",
+			inputURL: "http://blog.boot.dev/path/",
+			expectedURL: "blog.boot.dev/path",
+		},
 	}
 
-	for i, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			actual:= NormalizeURL(tc.inputURL)
-			// if err != nil {
-			// 	t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
-			// 	return
-			// }
-			if actual != tc.expected {
-				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
+	for i, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			normalizedURLs:= NormalizeURL(testCase.inputURL)
+
+			if normalizedURLs != testCase.expectedURL {
+				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, testCase.name, testCase.expectedURL, normalizedURLs)
 			}
 		})
 	}
